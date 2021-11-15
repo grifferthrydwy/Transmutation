aace088700a828f51cba4e0d82856c4af3ad7479
grifferthrydwy <78763622+grifferthrydwy@users.noreply.github.com>
2021-11-14 19:51:37 -0500

    Initial commit

---
+++ src/main/java/grifferthrydwy/transmutation/ItemTags.java
@@ -0,0 +1,12 @@
+//package grifferthrydwy.transmutation;
+
+//import net.fabricmc.fabric.api.tag.TagFactory;
+//import net.minecraft.block.Block;
+//import net.minecraft.item.Item;
+//import net.minecraft.tag.Tag;
+//import net.minecraft.util.Identifier;
+
+//public class BlockTags {
+//    public static final Tag<Item> Made_Of_Lead = TagFactory.ITEM.create(new Identifier(Main.MOD_ID, "made_of_lead"));
+//}
+
--- src/main/java/grifferthrydwy/transmutation/TransmutationRecipe.java
+++ src/main/java/grifferthrydwy/transmutation/TransmutationRecipe.java
@@ -1,17 +1,17 @@
-package grifferthrydwy.transmutation;
+//package grifferthrydwy.transmutation;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 
-public class TransmutationRecipe {
+//public class TransmutationRecipe {
 
-    if ( )
+    //if ( )
 
-    public int expCost() {
-        return 0;
-    }
-    public int newItem() {
-        return RegisterItems.RAW_LEAD;
-    }
+  //  public int expCost() {
+  //      return 0;
+  //  }
+  //  public int newItem() {
+ //       return RegisterItems.RAW_LEAD;
+ //   }
 
-}
+//}
