d39fc0246cc2260f8e2f42dc7032a2406b275020
SimplyCmd <simplycmd00@gmail.com>
2021-11-18 10:21:10 -0700

    Simplify transmutation recipes

--- src/main/java/grifferthrydwy/transmutation/TransmutationTable.java
+++ src/main/java/grifferthrydwy/transmutation/TransmutationTable.java
@@ -1,5 +1,7 @@
 package grifferthrydwy.transmutation;
 
+import java.util.HashMap;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
@@ -13,11 +15,19 @@ import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-import java.util.Objects;
-
 public class TransmutationTable extends Block {
-    private Object PlayerEntity;
-    private Object Hand;
+
+    // A list of input recipes and their outputs
+    private static final HashMap<Item, Item> RECIPES = new HashMap<>() {{
+        put(RegisterItems.LEAD_INGOT, RegisterItems.TRUE_GOLD_INGOT);
+        put(Items.ACACIA_LOG, Items.CRIMSON_STEM);
+        put(Items.SPRUCE_LOG, Items.ACACIA_LOG);
+        put(Items.DARK_OAK_LOG, Items.SPRUCE_LOG);
+        put(Items.BIRCH_LOG, Items.DARK_OAK_LOG);
+        put(Items.OAK_LOG, Items.BIRCH_LOG);
+        put(Items.CRIMSON_STEM, Items.WARPED_STEM);
+        put(Items.WARPED_STEM, Items.OAK_LOG);
+    }};
 
     public TransmutationTable(Settings settings) {
         super(settings);
@@ -27,19 +37,16 @@ public class TransmutationTable extends Block {
     public ActionResult onUse(BlockState blockState, World world, BlockPos blockPos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         if (player instanceof ServerPlayerEntity) {
             int xpneeded = requiredExperience(player, hand);
-            if ((player.totalExperience) > (xpneeded - 1)) {
-                final int itemCount = player.getStackInHand(hand).getCount();
+            if (player.totalExperience > xpneeded - 1 || player.isCreative()) {
                 player.addExperience((-xpneeded));
-                ItemStack result = (Objects.requireNonNull(resultItem(player, hand))).getDefaultStack();
-                result.setCount(itemCount);
+                ItemStack result = recipe(player.getStackInHand(hand).getItem()).getDefaultStack();
+                result.setCount(player.getStackInHand(hand).getCount());
                 player.setStackInHand(hand, result);
                 return ActionResult.SUCCESS;
-                } else {
-                    return ActionResult.FAIL;
-                }
-        } else {
-                return ActionResult.FAIL;
+            }
         }
+        // A replacement for the if else statements
+        return ActionResult.FAIL;
     }
 
 
@@ -51,26 +58,8 @@ public class TransmutationTable extends Block {
         }
     }
 
-    private Item resultItem(PlayerEntity player, Hand hand) {
-        if ((player.getStackInHand(hand).getItem() == RegisterItems.LEAD_INGOT)) {
-            return RegisterItems.TRUE_GOLD_INGOT;
-        } else if ((player.getStackInHand(hand).getItem() == Items.ACACIA_LOG)) {
-            return Items.CRIMSON_STEM;
-        } else if ((player.getStackInHand(hand).getItem() == Items.SPRUCE_LOG)) {
-            return Items.ACACIA_LOG;
-        } else if ((player.getStackInHand(hand).getItem() == Items.DARK_OAK_LOG)) {
-            return Items.SPRUCE_LOG;
-        } else if ((player.getStackInHand(hand).getItem() == Items.BIRCH_LOG)) {
-            return Items.DARK_OAK_LOG;
-        } else if ((player.getStackInHand(hand).getItem() == Items.OAK_LOG)) {
-            return Items.BIRCH_LOG;
-        } else if ((player.getStackInHand(hand).getItem() == Items.CRIMSON_STEM)) {
-            return Items.WARPED_STEM;
-        } else if ((player.getStackInHand(hand).getItem() == Items.WARPED_STEM)) {
-            return Items.OAK_LOG;
-        } else  {
-            return player.getStackInHand(hand).getItem();
-        }
-
+    private static Item recipe(Item input) {
+        // Looks up the input, and if it can't find a result, it returns the input.
+        return RECIPES.getOrDefault(input, input);
     }
 }
